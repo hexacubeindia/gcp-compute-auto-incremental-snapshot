6e232b6aff5a01b959571f9fb2e95ed61519c5d0
raman3366 <raman3366@gmail.com>
2018-08-12 15:37:29 +0530

    fixed incremental bug
    while deleting snapshots with timestamp greater than 31 days, in case of multiple snapshots, only single snapshot was deleted.
    This bug has been fixed. Now all snapshots with timestamp greater than 31 days will get deleted automatically.

--- snapshot.sh
+++ snapshot.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 # Author: L. Anantha Raman
-# Designation: CTO
+# Script Name: Do Snapshot
+# Script Description: Take live snapshots of all disks under a project. Gcloud init should be run for default account and project before execution.
 # Company: hexacube India
 # Website: www.hexacube.in
 # Phone: +91 9003314466
-#
-# Old Snapshot Deletion Starts
-#
-if [[ $(uname) == "Linux" ]]; then
-  from_date=$(date -d "-29 days" "+%Y-%m-%d")
-else
-  from_date=$(date -v -29d "+%Y-%m-%d")
-fi
-gcloud compute snapshots list --filter="creationTimestamp<$from_date" --uri | while read SNAPSHOT_URI; do
-   gcloud compute snapshots delete $SNAPSHOT_URI
-done
-#
-# Old Snapshot Deletion Ends
+# Email: info@hexacube.in
+# Last Updated: 2018-01-11
 #
 # Snapshot Creation Starts
 #
+starttime=$(date -d @$(( $(date +"%s") + 19800)) +"%Y-%m-%d-%H-%M")
+echo "Snapshot Creation Process started on $starttime"
+echo " "
+datetimestamp=$(date -d @$(( $(date +"%s") + 19800)) +"%Y-%m-%d---%H-%M-%S")
 gcloud compute disks list --format='value(name,zone)'| while read DISK_NAME ZONE; do
-  gcloud compute disks snapshot $DISK_NAME --snapshot-names autogcs-${DISK_NAME:0:31}-$(date "+%Y-%m-%d-%s") --zone $ZONE
+  gcloud compute disks snapshot $DISK_NAME --snapshot-names ${DISK_NAME:0:31}---$datetimestamp --zone $ZONE
 done
 #
 # Snapshot Creation Ends
+# Old Snapshot Deletion Starts
+#
+gcloud compute snapshots list --filter="creationTimestamp<$(date -d "-31 days" "+%Y-%m-%d")" --uri | while read -r SNAPSHOT_URI; do
+   gcloud compute snapshots delete "$SNAPSHOT_URI" --quiet
+done
+echo " "
+endtime=$(date -d @$(( $(date +"%s") + 19800)) +"%Y-%m-%d-%H-%M")
+echo "Snapshot Creation Process completed on $endtime"
+echo " "
+echo "List of available snapshots"
+echo " "
+gcloud compute snapshots list
+echo " "
+#
+# Old Snapshot Deletion Ends
+#
